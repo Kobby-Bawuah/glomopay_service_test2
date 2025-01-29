5d262b06f0f3a1b320a18bc893466e54e69c80b4
Kobby-Bawuah <Kobby.bawuah@sentry.io>
2025-01-29 14:34:53 -0500

    Add tests for multiply to increase coverage

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,5 +1,11 @@
-from src.calculator import add
+from src.calculator import add, multiply
 
 def test_add():
     assert add(2, 3) == 5
     assert add(-1, 1) == 0
+
+
+def test_multiply():
+    assert multiply(2, 3) == 6
+    assert multiply(0, 5) == 0
+    assert multiply(-1, 3) == -3
